3c7c83866c207b2a93aab3429aecf4a8e01ee34d
olh <ricardo@olh.me>
2021-08-11 06:25:18 -0300

    Update HeaderIndex.js

--- packages/gatsby-theme-try-ghost/src/components/common/HeaderIndex.js
+++ packages/gatsby-theme-try-ghost/src/components/common/HeaderIndex.js
@@ -30,8 +30,8 @@ const HeaderIndex = ({ data, overlay }) => {
                             )}
                         </h1>
                         <h2 className="site-description">{site.description}</h2>
+                        <h2><a class="subscribe-button" href="https://discord.com/oauth2/authorize?client_id=742668616779956245&permissions=133120&scope=bot">Invite Bot <i class="fab fa-discord"></i></a></h2>
                         <h2><img src="https://i.imgur.com/yVFl8Jx.png"></img></h2>
-                        <h2><a class="subscribe-button" href="https://discord.com/oauth2/authorize?client_id=742668616779956245&permissions=133120&scope=bot">Invite Bot</a></h2>
                     </div>
                 </div>
             </HeaderBackground>
